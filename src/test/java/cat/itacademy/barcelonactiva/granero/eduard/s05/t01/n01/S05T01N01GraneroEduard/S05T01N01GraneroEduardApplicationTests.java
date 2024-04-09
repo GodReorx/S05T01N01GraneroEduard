0d64c42ca21fadc133e25c91da7bdb42a6f3cc87
GodReorx <eduard.granero@gmail.com>
2024-04-09 17:21:30 +0200

    Creacion inicial

---
+++ src/test/java/cat/itacademy/barcelonactiva/granero/eduard/s05/t01/n01/S05T01N01GraneroEduard/S05T01N01GraneroEduardApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.granero.eduard.s05.t01.n01.S05T01N01GraneroEduard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N01GraneroEduardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
